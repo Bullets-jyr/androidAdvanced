fc3e48a0791ee987ebd31c667644e8dc3b592d90
Bullets-jyr <jyr@bullets.co.kr>
2023-01-14 20:32:55 +0900

    95. Retrofit Instance Class

---
+++ retrofitdemo/src/main/java/kr/co/bullets/retrofitdemo/RetrofitInstance.kt
@@ -0,0 +1,18 @@
+package kr.co.bullets.retrofitdemo
+
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitInstance {
+
+    companion object {
+        val BASE_URL = "https://jsonplaceholder.typicode.com/"
+        fun getRetrofitInstance(): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+                .build()
+        }
+    }
+}
