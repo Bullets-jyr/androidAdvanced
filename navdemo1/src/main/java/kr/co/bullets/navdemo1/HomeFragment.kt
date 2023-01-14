dbc6126c964193df24374ff695464fdb94861aaf
Bullets-jyr <jyr@bullets.co.kr>
2023-01-14 13:18:24 +0900

    42. Navigation Actions

--- navdemo1/src/main/java/kr/co/bullets/navdemo1/HomeFragment.kt
+++ navdemo1/src/main/java/kr/co/bullets/navdemo1/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import kr.co.bullets.navdemo1.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -17,6 +18,9 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        binding.button.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment)
+        }
 
         return binding.root
     }
