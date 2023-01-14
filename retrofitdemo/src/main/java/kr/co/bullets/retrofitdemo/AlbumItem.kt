24aa0e0951b7120ec9cebaa08bd0695c2e4314a3
Bullets-jyr <jyr@bullets.co.kr>
2023-01-14 20:21:42 +0900

    93. Create Data Classes

---
+++ retrofitdemo/src/main/java/kr/co/bullets/retrofitdemo/AlbumItem.kt
@@ -0,0 +1,12 @@
+package kr.co.bullets.retrofitdemo
+
+import com.google.gson.annotations.SerializedName
+
+data class AlbumItem(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("userId")
+    val userId: Int
+)
