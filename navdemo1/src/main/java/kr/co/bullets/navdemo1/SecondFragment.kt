c05e04a89b6608cf335eac695defcedd22c3c8a2
Bullets-jyr <jyr@bullets.co.kr>
2023-01-14 14:41:00 +0900

    43. Transforming Data Between Destinations

--- navdemo1/src/main/java/kr/co/bullets/navdemo1/SecondFragment.kt
+++ navdemo1/src/main/java/kr/co/bullets/navdemo1/SecondFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
 import kr.co.bullets.navdemo1.databinding.FragmentSecondBinding
 
@@ -17,6 +18,9 @@ class SecondFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+
+        var input = arguments?.getString("user_input") ?: ""
+        binding.textView.text = input.toString()
 
         return binding.root
     }
