61fbc1adc6cece88d5773066d2ce1c5949b7f392
Bullets-jyr <jyr@bullets.co.kr>
2023-01-16 00:52:59 +0900

    70. Data Access Object Interface(DAO)

---
+++ roomdemo/src/main/java/kr/co/bullets/roomdemo/db/SubscriberDAO.kt
@@ -0,0 +1,38 @@
+package kr.co.bullets.roomdemo.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface SubscriberDAO {
+
+    @Insert
+    suspend fun insertSubscriber(subscriber: Subscriber): Long
+
+//    @Insert
+//    fun insertSubscriber2(subscriber: Subscriber): Long
+
+//    @Insert
+//    fun insertSubscribers(subscriber1: Subscriber, subscriber2: Subscriber, subscriber3: Subscriber): List<Long>
+
+//    @Insert
+//    fun insertSubscribers(subscribers: List<Subscriber>): List<Long>
+
+//    @Insert
+//    fun insertSubscriber2(subscriber: Subscriber, subscribers: List<Subscriber>): List<Long>
+
+    @Update
+    suspend fun updateSubscriber(subscriber: Subscriber)
+
+    @Delete
+    suspend fun deleteSubscriber(subscriber: Subscriber)
+
+    @Query("DELETE FROM subscriber_data_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM subscriber_data_table")
+    fun getAllSubscribers(): LiveData<List<Subscriber>>
+
+//    @Query("SELECT * FROM subscriber_data_table")
+//    fun getAllSubscribers(): Flow<List<Subscriber>>
+}
