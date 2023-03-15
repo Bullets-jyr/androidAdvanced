943ddf60c208f9012dd2fd7f33890f40bb9a898a
Bullets-jyr <jyr@bullets.co.kr>
2023-03-16 08:17:14 +0900

    15. A Simple View Model Example

---
+++ viewmodeldemo1/src/main/java/kr/co/bullets/viewmodeldemo1/MainActivityViewModel.kt
@@ -0,0 +1,16 @@
+package kr.co.bullets.viewmodeldemo1
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+
+    private var count = 0
+
+    fun getCurrentCount(): Int {
+        return count
+    }
+
+    fun getUpdatedCount(): Int {
+        return ++count
+    }
+}
