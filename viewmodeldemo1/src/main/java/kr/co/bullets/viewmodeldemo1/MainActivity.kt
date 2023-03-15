b02ab1cfdf8ba068c86f62c8257e5cc20380e410
Bullets-jyr <jyr@bullets.co.kr>
2023-03-16 07:50:19 +0900

    14. Why Do We Need A View Model?

---
+++ viewmodeldemo1/src/main/java/kr/co/bullets/viewmodeldemo1/MainActivity.kt
@@ -0,0 +1,23 @@
+package kr.co.bullets.viewmodeldemo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.co.bullets.viewmodeldemo1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var count = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.countText.text = count.toString()
+        binding.button.setOnClickListener {
+            count++
+            binding.countText.text = count.toString()
+        }
+    }
+}
