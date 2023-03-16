cacf37b149462a3e548b107c566a94d842a5fce7
Bullets-jyr <jyr@bullets.co.kr>
2023-03-16 10:58:17 +0900

    24. LiveData Fundamentals

--- viewmodeldemo2/src/main/java/kr/co/bullets/viewmodeldemo2/MainActivity.kt
+++ viewmodeldemo2/src/main/java/kr/co/bullets/viewmodeldemo2/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.bullets.viewmodeldemo2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kr.co.bullets.viewmodeldemo2.databinding.ActivityMainBinding
 
@@ -19,11 +20,16 @@ class MainActivity : AppCompatActivity() {
         viewModelFactory = MainActivityViewModelFactory(125)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
 
-        binding.resultTextView.text = viewModel.getTotal().toString()
+//        viewModel.total.observe(this, Observer {
+        viewModel.totalData.observe(this, Observer {
+            binding.resultTextView.text = it.toString()
+        })
+
+//        binding.resultTextView.text = viewModel.getTotal().toString()
 
         binding.insertButton.setOnClickListener {
             viewModel.setTotal(binding.inputEditText.text.toString().toInt())
-            binding.resultTextView.text = viewModel.getTotal().toString()
+//            binding.resultTextView.text = viewModel.getTotal().toString()
         }
     }
 }
--- viewmodeldemo2/src/main/java/kr/co/bullets/viewmodeldemo2/MainActivityViewModel.kt
+++ viewmodeldemo2/src/main/java/kr/co/bullets/viewmodeldemo2/MainActivityViewModel.kt
@@ -1,20 +1,27 @@
 package kr.co.bullets.viewmodeldemo2
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel(startingTotal: Int) : ViewModel() {
 
-    private var total = 0
+//    private var total = 0
+    private var total = MutableLiveData<Int>()
+    val totalData: LiveData<Int>
+    get() = total
 
     init {
-        total = startingTotal
+//        total = startingTotal
+        total.value = startingTotal
     }
 
-    fun getTotal(): Int {
-        return total
-    }
+//    fun getTotal(): Int {
+//        return total
+//    }
 
     fun setTotal(input: Int) {
-        total += input
+//        total += input
+        total.value = (total.value)?.plus(input)
     }
 }
