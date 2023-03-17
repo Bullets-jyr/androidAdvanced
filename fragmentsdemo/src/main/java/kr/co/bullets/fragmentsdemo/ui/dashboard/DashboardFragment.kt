1473d5d448cfdde2f41a5e5047e055577d7c1f9c
Bullets-jyr <jyr@bullets.co.kr>
2023-03-17 12:05:47 +0900

    36. Fragments Introduction

---
+++ fragmentsdemo/src/main/java/kr/co/bullets/fragmentsdemo/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,48 @@
+package kr.co.bullets.fragmentsdemo.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import kr.co.bullets.fragmentsdemo.databinding.FragmentDashboardBinding
+
+class DashboardFragment : Fragment() {
+
+    private var _binding: FragmentDashboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+
+        binding.btnTest.setOnClickListener {
+            Toast.makeText(activity,"Clicked", Toast.LENGTH_LONG).show()
+        }
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
