af0defff8dab3fb020a12c1f8df054b595d7499d
Bullets-jyr <jyr@bullets.co.kr>
2023-03-17 14:58:50 +0900

    41. Navigation Destinations (Review)

---
+++ navdemo1review/src/main/java/kr/co/bullets/navdemo1review/HomeFragment.kt
@@ -0,0 +1,23 @@
+package kr.co.bullets.navdemo1review
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import kr.co.bullets.navdemo1review.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+}
---
+++ navdemo1review/src/main/java/kr/co/bullets/navdemo1review/SecondFragment.kt
@@ -0,0 +1,23 @@
+package kr.co.bullets.navdemo1review
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import kr.co.bullets.navdemo1review.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_second, container, false)
+        return binding.root
+    }
+}
