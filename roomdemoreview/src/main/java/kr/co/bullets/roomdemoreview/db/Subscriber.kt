9b50f274d946fd8cd90a590f20ca2ffea18d8137
Bullets-jyr <jyr@bullets.co.kr>
2023-03-19 20:35:00 +0900

    73. Design The View (Review)

--- roomdemoreview/src/main/java/kr/co/bullets/roomdemoreview/db/Subscriber.kt
+++ roomdemoreview/src/main/java/kr/co/bullets/roomdemoreview/db/Subscriber.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "subscriber_data")
+@Entity(tableName = "subscriber_data_table")
 data class Subscriber(
 
     @PrimaryKey(autoGenerate = true)
