68c2a0d8dbabc5b656b252e78cf77c765a276d1b
Bullets-jyr <jyr@bullets.co.kr>
2023-03-19 20:25:13 +0900

    72. Repository In MVVM (Review)

--- roomdemoreview/src/main/java/kr/co/bullets/roomdemoreview/db/SubscriberDatabase.kt
+++ roomdemoreview/src/main/java/kr/co/bullets/roomdemoreview/db/SubscriberDatabase.kt
@@ -10,12 +10,18 @@ abstract class SubscriberDatabase : RoomDatabase() {
 
     abstract val subscriberDAO: SubscriberDAO
 
+    // Kotlin에서는 객체지향 디자인 패턴 중 하나인 싱글톤 패턴을 쉽게 구현하기 위해 companion object를 제공합니다.
+    // companion object 키워드로 선언된 객체는 해당 클래스의 인스턴스를 생성하지 않아도 접근 가능합니다.
     companion object {
 
+        // @Volatile을 사용하면 해당 변수가 캐시를 거치지 않고 항상 메인 메모리에서 값을 읽고 쓰기 때문에 스레드 안전성이 보장됩니다.
+        // 멀티 스레드 환경에서도 안전하게 값을 읽고 쓸 수 있습니다. 즉, 하나의 스레드가 값을 변경하더라도, 다른 스레드에서는 항상 최신 값을 읽을 수 있습니다.
+        // 이는 멀티스레드 환경에서 변수의 값을 안전하게 관리하기 위한 것으로, 변수의 값을 항상 메인 메모리에서 읽고 쓰도록 보장합니다.
         @Volatile
         private var INSTANCE: SubscriberDatabase? = null
 
         fun getInstance(context: Context): SubscriberDatabase {
+            // synchronized 블록을 사용하여 메소드에 대한 동기화를 보장합니다. 이 블록 내부에서는 한 번에 하나의 스레드만이 실행됩니다.
             synchronized(this) {
                 var instance = INSTANCE
 
---
+++ roomdemoreview/src/main/java/kr/co/bullets/roomdemoreview/db/SubscriberRepository.kt
@@ -0,0 +1,22 @@
+package kr.co.bullets.roomdemoreview.db
+
+class SubscriberRepository(private val dao: SubscriberDAO) {
+
+    val subscribers = dao.getAllSubscribers()
+
+    suspend fun insert(subscriber: Subscriber): Long {
+        return dao.insertSubscriber(subscriber)
+    }
+
+    suspend fun update(subscriber: Subscriber): Int {
+        return dao.updateSubscriber(subscriber)
+    }
+
+    suspend fun delete(subscriber: Subscriber): Int {
+        return dao.deleteSubscriber(subscriber)
+    }
+
+    suspend fun deleteAll(): Int {
+        return dao.deleteAll()
+    }
+}
