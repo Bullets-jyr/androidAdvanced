d614c36c97b56d5a325a01b60ae9af466ac715f9
Bullets-jyr <jyr@bullets.co.kr>
2023-03-19 17:28:17 +0900

    71. Room Database Class (Review)

---
+++ roomdemoreview/src/main/java/kr/co/bullets/roomdemoreview/db/SubscriberDatabase.kt
@@ -0,0 +1,35 @@
+package kr.co.bullets.roomdemoreview.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Subscriber::class], version = 1)
+abstract class SubscriberDatabase : RoomDatabase() {
+
+    abstract val subscriberDAO: SubscriberDAO
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: SubscriberDatabase? = null
+
+        fun getInstance(context: Context): SubscriberDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SubscriberDatabase::class.java,
+                        "subscriber_data_database"
+                    ).build()
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+    }
+}
