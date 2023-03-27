8f39d42d49f4c43239a64c7bdded99eff76f07d8
Bullets-jyr <jyr@bullets.co.kr>
2023-03-27 12:08:27 +0900

    79. Verification With Returned Values From Database (Review)

--- roomdemoreview/src/main/java/kr/co/bullets/roomdemoreview/SubscriberViewModel.kt
+++ roomdemoreview/src/main/java/kr/co/bullets/roomdemoreview/SubscriberViewModel.kt
@@ -55,7 +55,7 @@ class SubscriberViewModel(private val repository: SubscriberRepository) : ViewMo
         }
     }
 
-    fun insert(subscriber: Subscriber) {
+    private fun insert(subscriber: Subscriber) {
         // viewModelScope는 ViewModel에서 코루틴을 사용할 때 사용하는 CoroutineScope입니다.
         // viewModelScope를 사용하면 ViewModel 생명주기에 따라서 코루틴을 실행하고,
         // ViewModel이 소멸될 때 코루틴도 함께 취소됩니다. 이는 앱에서 비동기 처리를 할 때 매우 유용합니다.
@@ -63,9 +63,13 @@ class SubscriberViewModel(private val repository: SubscriberRepository) : ViewMo
         // 액티비티가 백그라운드로 이동하여 화면이 회전되는 등의 상황에서도 코루틴이 취소되어 메모리 누수나 오작동 등의 문제를 예방할 수 있습니다.
         // 따라서, viewModelScope를 사용하면 ViewModel에서 비동기 처리를 할 때 안정적이고 쉽게 구현할 수 있으며, 안드로이드 앱의 성능과 안정성을 향상시킬 수 있습니다.
         viewModelScope.launch(Dispatchers.IO) {
-            repository.insert(subscriber)
+            val newRowId = repository.insert(subscriber)
             withContext(Dispatchers.Main) {
-                statusMessage.value = Event("Subscriber Inserted Successfully!")
+                if (newRowId > -1) {
+                    statusMessage.value = Event("Subscriber Inserted Successfully! $newRowId")
+                } else {
+                    statusMessage.value = Event("Error Occurred!")
+                }
             }
         }
     }
@@ -74,35 +78,47 @@ class SubscriberViewModel(private val repository: SubscriberRepository) : ViewMo
 //        repository.insert(subscriber)
 //    }
 
-    fun update(subscriber: Subscriber) = viewModelScope.launch(Dispatchers.IO) {
-        repository.update(subscriber)
+    private fun update(subscriber: Subscriber) = viewModelScope.launch(Dispatchers.IO) {
+        val numberOfRows = repository.update(subscriber)
         withContext(Dispatchers.Main) {
-            inputName.value = ""
-            inputEmail.value = ""
-            isUpdateOrDelete = false
-            saveOrUpdateButtonText.value = "Save"
-            clearAllOrDeleteButtonText.value = "Clear All"
-            statusMessage.value = Event("Subscriber Updated Successfully!")
+            if (numberOfRows > 0) {
+                inputName.value = ""
+                inputEmail.value = ""
+                isUpdateOrDelete = false
+                saveOrUpdateButtonText.value = "Save"
+                clearAllOrDeleteButtonText.value = "Clear All"
+                statusMessage.value = Event("$numberOfRows Rows Subscriber Updated Successfully!")
+            } else {
+                statusMessage.value = Event("Error Occurred!")
+            }
         }
     }
 
-    fun delete(subscriber: Subscriber) = viewModelScope.launch(Dispatchers.IO) {
-        repository.delete(subscriber)
+    private fun delete(subscriber: Subscriber) = viewModelScope.launch(Dispatchers.IO) {
+        val numberOfRowsDeleted = repository.delete(subscriber)
         // Since we are going to directly deal with the UI, we need to change the thread using withContext()
         withContext(Dispatchers.Main) {
-            inputName.value = ""
-            inputEmail.value = ""
-            isUpdateOrDelete = false
-            saveOrUpdateButtonText.value = "Save"
-            clearAllOrDeleteButtonText.value = "Clear All"
-            statusMessage.value = Event("Subscriber Deleted Successfully!")
+            if (numberOfRowsDeleted > 0) {
+                inputName.value = ""
+                inputEmail.value = ""
+                isUpdateOrDelete = false
+                saveOrUpdateButtonText.value = "Save"
+                clearAllOrDeleteButtonText.value = "Clear All"
+                statusMessage.value = Event("$numberOfRowsDeleted Rows Subscriber Deleted Successfully!")
+            } else {
+                statusMessage.value = Event("Error Occurred!")
+            }
         }
     }
 
-    fun clearAll() = viewModelScope.launch(Dispatchers.IO) {
-        repository.deleteAll()
+    private fun clearAll() = viewModelScope.launch(Dispatchers.IO) {
+        val numberOfRowsDeleted = repository.deleteAll()
         withContext(Dispatchers.Main) {
-            statusMessage.value = Event("All Subs Successfully!")
+            if (numberOfRowsDeleted > 0) {
+                statusMessage.value = Event("$numberOfRowsDeleted Rows All Subs Deleted Successfully!")
+            } else {
+                statusMessage.value = Event("Error Occurred!")
+            }
         }
     }
 
