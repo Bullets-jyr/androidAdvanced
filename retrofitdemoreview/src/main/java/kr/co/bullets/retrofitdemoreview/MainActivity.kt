85f0c7455a2e502603a77d7d0943bfb9875de6bf
Bullets-jyr <jyr@bullets.co.kr>
2023-03-27 19:36:14 +0900

    96. View Data (Review)

--- retrofitdemoreview/src/main/java/kr/co/bullets/retrofitdemoreview/MainActivity.kt
+++ retrofitdemoreview/src/main/java/kr/co/bullets/retrofitdemoreview/MainActivity.kt
@@ -2,10 +2,38 @@ package kr.co.bullets.retrofitdemoreview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val text_view = findViewById<TextView>(R.id.text_view)
+        val retService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+        val responseLiveData: LiveData<Response<Albums>> = liveData {
+            val response = retService.getAlbums()
+            emit(response)
+        }
+
+        responseLiveData.observe(this, Observer {
+            // 이 메서드는 ListIterator 인터페이스를 반환하는데, 이 인터페이스는 List를 양방향으로 순회하며 수정할 수 있는 기능을 제공합니다.
+            val albumsList = it.body()?.listIterator()
+            Log.e("MYTAG", "$albumsList")
+            if (albumsList != null) {
+                while (albumsList.hasNext()) {
+                    val albumsItem = albumsList.next()
+                    Log.i("MYTAG", albumsItem.title)
+                    val result = " " + "Album title : ${albumsItem.title}" + "\n" +
+                                 " " + "Album id : ${albumsItem.id}" + "\n" +
+                                 " " + "Album userId : ${albumsItem.userId}" + "\n\n\n"
+                    text_view.append(result)
+                }
+            }
+        })
     }
 }
