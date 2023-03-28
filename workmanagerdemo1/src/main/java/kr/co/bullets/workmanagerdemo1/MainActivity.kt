022f85040fbcdce3cf21ab07b46af60e8de6ac50
Bullets-jyr <jyr@bullets.co.kr>
2023-03-28 14:05:20 +0900

    111. Set Input & Output Data Of A Worker Class

--- workmanagerdemo1/src/main/java/kr/co/bullets/workmanagerdemo1/MainActivity.kt
+++ workmanagerdemo1/src/main/java/kr/co/bullets/workmanagerdemo1/MainActivity.kt
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.work.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val KEY_COUNT_VALUE = "key_count"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,6 +23,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun setOneTimeWorkRequest() {
         val workManager = WorkManager.getInstance(applicationContext)
+
+        val data = Data.Builder()
+            .putInt(KEY_COUNT_VALUE, 125)
+            .build()
+
         val constraints = Constraints.Builder()
             .setRequiresCharging(true)
             .setRequiredNetworkType(NetworkType.CONNECTED)
@@ -26,12 +35,19 @@ class MainActivity : AppCompatActivity() {
 
         val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
             .setConstraints(constraints)
+            .setInputData(data)
             .build()
 
         workManager.enqueue(uploadRequest)
         workManager.getWorkInfoByIdLiveData(uploadRequest.id)
             .observe(this, Observer {
                 val textView = findViewById<TextView>(R.id.textView)
+                // We cannot get a returned value if the work is not finished.
+                if (it.state.isFinished) {
+                    val data = it.outputData
+                    val message = data.getString(UploadWorker.KEY_WORKER)
+                    Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+                }
                 textView.text = it.state.name
             })
     }
--- workmanagerdemo1/src/main/java/kr/co/bullets/workmanagerdemo1/UploadWorker.kt
+++ workmanagerdemo1/src/main/java/kr/co/bullets/workmanagerdemo1/UploadWorker.kt
@@ -2,18 +2,34 @@ package kr.co.bullets.workmanagerdemo1
 
 import android.content.Context
 import android.util.Log
+import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class UploadWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
 
+    companion object {
+        const val KEY_WORKER = "key_worker"
+    }
     override fun doWork(): Result {
         try {
-            for (i in 0..600000) {
+            val count = inputData.getInt(MainActivity.KEY_COUNT_VALUE, 0)
+
+//            for (i in 0..600000) {
+            for (i in 0 until count) {
                 Log.i("MYTAG", "Uploading $i")
             }
 
-            return Result.success()
+            val time = SimpleDateFormat("dd/M/yyyy hh:mm")
+            val currentDate = time.format(Date())
+
+            val outPutData = Data.Builder()
+                .putString(KEY_WORKER, currentDate)
+                .build()
+
+            return Result.success(outPutData)
         } catch (e: java.lang.Exception) {
             return Result.failure()
         }
