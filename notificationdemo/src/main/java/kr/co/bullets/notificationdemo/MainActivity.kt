a0d6fc926b7a0701027dbf7df87ea39faa3db055
Bullets-jyr <jyr@bullets.co.kr>
2023-03-28 09:05:36 +0900

    104. Tap action

--- notificationdemo/src/main/java/kr/co/bullets/notificationdemo/MainActivity.kt
+++ notificationdemo/src/main/java/kr/co/bullets/notificationdemo/MainActivity.kt
@@ -2,8 +2,10 @@ package kr.co.bullets.notificationdemo
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.ClipDescription
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -29,12 +31,24 @@ class MainActivity : AppCompatActivity() {
 
     private fun displayNotification() {
         val notificationId = 45
+        val tapResultIntent = Intent(this, SecondActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            tapResultIntent,
+//            PendingIntent.FLAG_UPDATE_CURRENT,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
         val notification = NotificationCompat.Builder(this@MainActivity, channelID)
             .setContentTitle("Demo Title")
             .setContentText("This is a demo notification")
             .setSmallIcon(android.R.drawable.ic_dialog_info)
             .setAutoCancel(true)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
             .build()
         notificationManager?.notify(notificationId, notification)
     }
---
+++ notificationdemo/src/main/java/kr/co/bullets/notificationdemo/SecondActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.bullets.notificationdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
